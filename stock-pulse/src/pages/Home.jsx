6c3093219af28ebd269fcc9b5128d9f5958baabf
Shatayu Akare <shatayuakare121@outlook.com>
2024-08-13 19:45:17 +0530

    fixing issue

--- stock-pulse/src/pages/Home.jsx
+++ stock-pulse/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Team from "../components/about/Team";
-import Main from '../components/home/Main';
+// import Main from '../components/home/Main';
 import FAQ from '../components/about/FAQ';
 import Contact from '../components/home/Contact1';
 import Hero from '../components/contact/Hero';
@@ -8,7 +8,7 @@ import Hero from '../components/contact/Hero';
 const Home = () => {
     return (
         <section className="bg-[url('https://img.freepik.com/free-vector/gradient-minimalist-background_23-2149974337.jpg')] bg-cover bg-center p-0 bg-fixed">
-            <Main />
+            {/* <Main /> */}
             <Hero />
             <Team />
             <Contact />
