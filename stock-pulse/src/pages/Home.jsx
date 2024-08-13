241299b3f7c58c38c57a5c437beaf8c91291b513
Shatayu Akare <shatayuakare121@outlook.com>
2024-08-13 23:52:02 +0530

    Fixed

--- stock-pulse/src/pages/Home.jsx
+++ stock-pulse/src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Team from "../components/about/Team";
-// import Main from '../components/home/Main';
-import FAQ from '../components/about/FAQ';
+import Main from '../components/home/Main';
 import Contact from '../components/home/Contact1';
+import Team from "../components/about/Team";
+import FAQ from '../components/about/FAQ';
 import Hero from '../components/contact/Hero';
 
 const Home = () => {
     return (
         <section className="bg-[url('https://img.freepik.com/free-vector/gradient-minimalist-background_23-2149974337.jpg')] bg-cover bg-center p-0 bg-fixed">
-            {/* <Main /> */}
+            <Main />
             <Hero />
             <Team />
             <Contact />
