7c26edd828972762c72dd8b85041cd36bb96efe5
Shatayu Akare <shatayuakare121@outlook.com>
2024-08-13 19:42:08 +0530

    fixing miner issue

--- stock-pulse/src/pages/Home.jsx
+++ stock-pulse/src/pages/Home.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import Team from "../components/about/Team";
-import HomeMain from '../components/home/HomeMain';
+import Main from '../components/home/Main';
 import FAQ from '../components/about/FAQ';
 import Contact from '../components/home/Contact1';
 import Hero from '../components/contact/Hero';
 
 const Home = () => {
     return (
-
-
         <section className="bg-[url('https://img.freepik.com/free-vector/gradient-minimalist-background_23-2149974337.jpg')] bg-cover bg-center p-0 bg-fixed">
-            <HomeMain />
+            <Main />
             <Hero />
             <Team />
             <Contact />
